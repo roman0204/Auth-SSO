6957f92d3525ff7bcb51e09ca04dd1189d9be1e3
unknown <romann0204@gmail.com>
2022-10-05 14:45:17 +0300

    Add Internal

---
+++ internal/templates/const.go
@@ -0,0 +1,15 @@
+package templates
+
+// Template File Names.
+const (
+	TemplateNameEmailEnvelope                 = "Envelope.tmpl"
+	TemplateNameEmailIdentityVerificationHTML = "IdentityVerification.html"
+	TemplateNameEmailIdentityVerificationTXT  = "IdentityVerification.txt"
+	TemplateNameEmailPasswordResetHTML        = "PasswordReset.html"
+	TemplateNameEmailPasswordResetTXT         = "PasswordReset.txt"
+)
+
+// Template Category Names.
+const (
+	TemplateCategoryNotifications = "notification"
+)
